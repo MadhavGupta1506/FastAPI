3d2bee590713072a9f2db73069ac64db8a08819b
MadhavGupta1506 <gmadhav857@gmail.com>
2025-01-12 15:58:15 +0530

    Get complete

--- main.py
+++ main.py
@@ -1,15 +1,29 @@
 from fastapi import FastAPI
-
+from typing import Optional
 app= FastAPI()
 
 @app.get("/")
 def index():
     return {"Data":"madhav"}
 
-@app.get("/about")
-def index():
-    return "Madhav"
 
+# Getting all the Blogs
+# @app.get()
+# def index():
+#     return {"data":"All Blogs"}
+
+
+# Getting limited blogs
+@app.get("/blog")
+# Using query parameters
+def index(limit,published:bool,sort:Optional[str]=None):
+    if(published):
+        return {"data":f"{limit} Blogs from data base"}
+    return {"data":"all Blogs from data base"}
+
+@app.get("/blog/unpublished")
+def unpublished():
+    return {'data':"unpublished"}
 
 # Fetch data according to some id
 @app.get("/blog/{id}")
@@ -17,5 +31,6 @@ def show(id:int):
     return {'data': id}
 
 @app.get("/blog/{id}/comments")
-def comments(id:int):
-    return {"data":{"comments":{'1','2','3','4'}}}
+def comments(id:int,limit=10):
+    # return limit
+    return {id:{"comments":("1","2","3")}}
